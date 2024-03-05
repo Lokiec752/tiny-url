28286e35f4860959bea8411f5d5058ed6e034a49
marcin <marcin.gradzki.dev@gmail.com>
2024-03-05 16:41:06 +0100

    Add .env to .gitignore and implement Google OAuth router

--- be/src/index.ts
+++ be/src/index.ts
@@ -2,8 +2,9 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 import userRouter from "./routers/user";
+import googleRouter from "./routers/google";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const app: Express = express();
 const port = process.env.PORT || 3333;
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 
 app.use("/api/users", userRouter);
+app.use("/api/google", googleRouter);
 
 // GET short url
 app.get("/", async (req: Request, res: Response) => {
---
+++ be/src/routers/google.ts
@@ -0,0 +1,27 @@
+import express from "express";
+
+const googleRouter = express.Router();
+
+googleRouter.get("/", async (req, res) => {
+  const rootUrl = "https://www.googleapis.com/oauth2/v2/auth";
+
+  const options = {
+    client_id: process.env.CLIENT_ID as string,
+    redirect_uri: process.env.REDIRECT_URI as string,
+    access_type: "offline",
+    response_type: "code",
+    prompt: "consent",
+    scopes: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" "),
+  };
+
+  const qs = new URLSearchParams(options).toString();
+
+  const googleAuthUrl = `${rootUrl}?${qs.toString()}`;
+
+  res.json({ googleAuthUrl });
+});
+
+export default googleRouter;
