c7d5db6736cca26cd252c824ba2541616ea2d700
marcin <marcin.gradzki.dev@gmail.com>
2024-03-07 13:45:32 +0100

    Add session table

--- be/src/db/schema.ts
+++ be/src/db/schema.ts
@@ -1,4 +1,9 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import {
+  SQLiteBoolean,
+  integer,
+  sqliteTable,
+  text,
+} from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: text("id").primaryKey(),
@@ -15,8 +20,19 @@ export const links = sqliteTable("links", {
   created_at: text("created_at"),
 });
 
+export const session = sqliteTable("session", {
+  id: text("id").primaryKey(),
+  user_id: text("user_id").references(() => users.id),
+  valid: integer("valid", { mode: "boolean" }),
+  created_at: text("created_at"),
+  updated_at: text("updated_at"),
+});
+
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
 
 export type Link = typeof links.$inferSelect;
 export type NewLink = typeof links.$inferInsert;
+
+export type Session = typeof session.$inferSelect;
+export type NewSession = typeof session.$inferInsert;
