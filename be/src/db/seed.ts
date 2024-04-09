21c03daa314df26cd8396991ed12834d1ca47782
marcin <marcin.gradzki.dev@gmail.com>
2024-04-09 12:48:45 +0200

    Update seed.ts and compose.yaml files

--- be/src/db/seed.ts
+++ be/src/db/seed.ts
@@ -1,6 +1,5 @@
 import { createUser } from "../service/user.service";
 import { drizzle } from "drizzle-orm/mysql2";
-import { migrate } from "drizzle-orm/mysql2/migrator";
 import mysql from "mysql2/promise";
 
 const connectionPromise = mysql.createConnection({
